57cdb8b3285ac42c08cc35c81efc9fc56360c0bd
leiwingqueen <leiwingqueen@gmail.com>
2023-01-03 17:42:19 +0800

    catelog implement

--- golang/src/catalog/catelog.go
+++ golang/src/catalog/catelog.go
@@ -3,8 +3,11 @@ package catalog
 import "errors"
 
 //  alias
-type DatabaseId = int
-type SchemaId = int
+type DatabaseId = int32
+type SchemaId = int32
+type TableId = int32
+type ColumnId = int32
+type DataType = int32
 
 // DatabaseCatalog :Definition for DatabaseCatalog
 type DatabaseCatalog struct {
@@ -18,16 +21,16 @@ type DatabaseCatalog struct {
 }
 
 func ConstructDataBase(id DatabaseId, name string) *DatabaseCatalog {
-	return &DatabaseCatalog{id, name, 1, make(map[SchemaId]*SchemaCatalog), make(map[string]SchemaId)}
+	return &DatabaseCatalog{id, name, 0, make(map[SchemaId]*SchemaCatalog), make(map[string]SchemaId)}
 }
 
 func (db *DatabaseCatalog) addSchema(name string) (SchemaId, error) {
 	if _, exist := db.nameIdMapping[name]; exist {
-		return 0, errors.New("name already exist")
+		return -1, errors.New("name already exist")
 	}
 	schemaId := db.nextSchemeId
-	schema := SchemaCatalog{id: schemaId, name: name}
-	db.schemas[schemaId] = &schema
+	schema := constructSchema(schemaId, name)
+	db.schemas[schemaId] = schema
 	db.nextSchemeId++
 	return schemaId, nil
 }
@@ -49,10 +52,84 @@ func (db DatabaseCatalog) delSchema(id SchemaId) error {
 	return nil
 }
 
+// SchemaCatalog :Definition for Schema
 type SchemaCatalog struct {
-	id   SchemaId
-	name string
+	id          SchemaId
+	name        string
+	nextTableId TableId
+	tables      map[TableId]*TableCatalog
+	// simple mapping for name to id, check duplicate name
+	nameIdMapping map[string]TableId
 }
 
-type attribute struct {
+func constructSchema(id SchemaId, name string) *SchemaCatalog {
+	return &SchemaCatalog{id, name, 0, make(map[TableId]*TableCatalog), make(map[string]TableId)}
+}
+
+func (schema *SchemaCatalog) addTable(name string, cols []ColumnCatalog) (TableId, error) {
+	if _, exist := schema.nameIdMapping[name]; exist {
+		return -1, errors.New("table already exist")
+	}
+	tableId := schema.nextTableId
+	schema.nextTableId++
+	table := constructTable(tableId, name, cols)
+	schema.tables[tableId] = table
+	schema.nameIdMapping[name] = tableId
+	return tableId, nil
+}
+
+func (schema *SchemaCatalog) getTable(id TableId) (*TableCatalog, error) {
+	if _, exist := schema.tables[id]; !exist {
+		return nil, errors.New("table not exist")
+	}
+	return schema.tables[id], nil
+}
+
+func (schema *SchemaCatalog) delTable(id TableId) error {
+	if _, exist := schema.tables[id]; !exist {
+		return errors.New("table not exist")
+	}
+	table := schema.tables[id]
+	delete(schema.tables, id)
+	delete(schema.nameIdMapping, table.name)
+	return nil
+}
+
+// TableCatalog :Definition for Table
+type TableCatalog struct {
+	id   TableId
+	name string
+	cols []ColumnCatalog
+}
+
+func constructTable(id TableId, name string, cols []ColumnCatalog) *TableCatalog {
+	// generate column id
+	var colId ColumnId
+	colId = 0
+	for _, col := range cols {
+		col.id = colId
+		colId++
+	}
+	return &TableCatalog{id, name, cols}
+}
+
+func (tb *TableCatalog) getColumn(id ColumnId) (*ColumnCatalog, error) {
+	for _, col := range tb.cols {
+		if col.id == id {
+			return &col, nil
+		}
+	}
+	return nil, errors.New("column not exist")
+}
+
+type ColumnCatalog struct {
+	id   ColumnId
+	name string
+	desc ColumnDesc
+}
+
+type ColumnDesc struct {
+	nullable bool
+	primary  bool
+	dataType DataType
 }
